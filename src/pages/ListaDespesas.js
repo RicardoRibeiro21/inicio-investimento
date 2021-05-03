19ac385c23db6399ca3315d55afcd27ad642ee67
Ricardo Ribeiro Lopes <ricardo.sao@hotmail.com>
2021-05-03 20:13:48 -0300

    Refresh na página quando excluí algum item

--- src/pages/ListaDespesas.js
+++ src/pages/ListaDespesas.js
@@ -5,13 +5,21 @@ import * as S from './listaDespesa-styled';
 const getListLocalStorage = () => {
     return JSON.parse(localStorage.getItem('listaDespesas'))
 }
-console.log('Carrega antes do component')
+
+const setListLocalStorage = (list) => {
+    console.log(list)
+    localStorage.setItem('listaDespesas', JSON.stringify(list))
+    window.location.reload();
+}
+
 export default function ListaDespesas() {
-    const [listDespesas, setListDespesas] = useState([]);
+    const [listDespesas, setListDespesas] = useState(null);
     const history = useHistory();
-    console.log('Carrega depois do component')
+
     useEffect(() => {
+        console.log('1', getListLocalStorage())
         setListDespesas(getListLocalStorage())
+        console.log('2', listDespesas)
         console.log('Está carregando', getListLocalStorage())
     }, [])
 
@@ -20,21 +28,17 @@ export default function ListaDespesas() {
     //     console.log('Está carregando', getListLocalStorage())
     // }, [listDespesas])
 
-
-
     function handleDelete(index, month) {
         console.log('State Antes de excluir', listDespesas)
         console.log('LocalStorage antes de excluir', getListLocalStorage())
-
         setListDespesas(listDespesas.map(item => {
             if (item.month == month) {
                 // Removendo item do array
                 item.items.splice(index, 1);
             }
         }))
-        localStorage.setItem('listaDespesas', JSON.stringify(listDespesas))
+        setListLocalStorage(listDespesas)
         console.log('Salvou localStorage', getListLocalStorage())
-        setListDespesas(getListLocalStorage())
         console.log('Salvou state', listDespesas)
     }
 
@@ -44,61 +48,38 @@ export default function ListaDespesas() {
     }
 
     function retornarConteudo() {
-        return (
-            listDespesas.map(item => {
-                let totMes = 0
-                return (
-                    <S.Month>
-                        <S.MonthName>{item.month} </S.MonthName>
-                        {
-                            item.items.map((desp, index) => {
-                                totMes += parseInt(desp.price);
-                                return (
-                                    <S.Item>
-                                        <S.Props>{desp.description}</S.Props>
-                                        <S.Props>{desp.price}</S.Props>
-                                        <S.BtnRemover onClick={() => handleDelete(index, item.month)}>Remover</S.BtnRemover>
-                                    </S.Item>)
-                            })
-                        }
-                        {totMes > 0 ? <S.TotValue>Valor total: {totMes}</S.TotValue> : ''}
-                    </S.Month>
-                )
-
-            })
-        )
+        if (listDespesas) {
+            return (
+                listDespesas.map(item => {
+                    let totMes = 0
+                    return (
+                        <S.Month>
+                            <S.MonthName>{item.month} </S.MonthName>
+                            {
+                                item.items.map((desp, index) => {
+                                    totMes += parseInt(desp.price);
+                                    return (
+                                        <S.Item>
+                                            <S.Props>{desp.description}</S.Props>
+                                            <S.Props>{desp.price}</S.Props>
+                                            <S.BtnRemover onClick={() => handleDelete(index, item.month)}>Remover</S.BtnRemover>
+                                        </S.Item>)
+                                })
+                            }
+                            {totMes > 0 ? <S.TotValue>Valor total: {totMes}</S.TotValue> : ''}
+                        </S.Month>
+                    )
+                })
+            )
+        }
+        return <h1>Cade sua lista seu otareo</h1>
     }
 
     return (
         <S.Container>
             <S.H1> Lista de Despesas </S.H1>
             <S.List>
-                {
-                    // retornarConteudo() != null ?
-                    retornarConteudo()
-                    //  :
-                    // listDespesas.map(item => {
-                    //     let totMes = 0
-                    //     return (
-                    //         <S.Month>
-                    //             <S.MonthName>{item.month} </S.MonthName>
-                    //             {
-                    //                 item.items.map((desp, index) => {
-                    //                     totMes += parseInt(desp.price);
-                    //                     return (
-                    //                         <S.Item>
-                    //                             <S.Props>{desp.description}</S.Props>
-                    //                             <S.Props>{desp.price}</S.Props>
-                    //                             <S.BtnRemover onClick={() => handleDelete(index, item.month)}>Remover</S.BtnRemover>
-                    //                         </S.Item>)
-                    //                 })
-                    //             }
-                    //             {totMes > 0 ? <S.TotValue>Valor total: {totMes}</S.TotValue> : ''}
-                    //         </S.Month>
-                    //     )
-
-                    // })
-                }
+                {retornarConteudo()}
             </S.List>
             <S.BtnBack onClick={handleToPage}>Voltar para Cadastro</S.BtnBack>
         </S.Container>
